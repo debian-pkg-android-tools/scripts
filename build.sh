6d8cb236479a62bfe37d6ad8f4e16daec1df1e1a
Chirayu Desai <chirayudesai1@gmail.com>
2016-07-21 22:24:20 +0530

    Add build script
    * Builds all of the current android-tools repos using gbp.
    * --git-no-pristine-tar is passed to gbp since all we want is to
      build a package.

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Script to update android-tools repos to the latest upstream.
+#
+# Chirayu Desai
+# Android Tools Maintainers
+
+# Assumes you have the below repos cloned to a directory with the same name.
+# Runs git clean, so you sure you don't have any unsaved work.
+
+# From https://wiki.debian.org/AndroidTools#Updating_the_source_packages
+repos='
+android-platform-external-libunwind
+android-platform-external-libselinux
+android-platform-build_stage1
+android-platform-frameworks-native
+android-platform-libcore
+android-platform-system-core_stage1
+android-platform-system-extras
+android-platform-libnativehelper
+android-platform-frameworks-base
+android-platform-dalvik
+android-platform-build
+android-platform-system-core
+'
+
+# $1 repo
+# $2 stage, empty by default
+function build() {
+	export DEB_BUILD_PROFILES=${2}
+	cd ${1}
+	gbp buildpackage --git-no-pristine-tar
+	cd ..
+}
+
+function build_all() {
+	for repo in ${repos}; do
+		build ${repo%_*} ${repo#*_}
+	done
+}
+
+build_all
